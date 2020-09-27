2ea03ca00e3ab1e38ce67f2c19045639a67d0570
Randud <randu612@gmail.com>
2020-09-27 13:15:09 +0530

    Generate Group Id function added

---
+++ src/api/generatedGroups/generatedGroup.controller.ts
@@ -0,0 +1,114 @@
+import { Request, Response } from "express";
+import * as mongodb from "mongodb";
+import * as responses from "../../helpers/responses.handler";
+import ErrorCodes from "../../config/error.codes";
+import SuccessCodes from "../../config/success.codes";
+import { MongoHelper } from "../../config/mongodb.config";
+import GeneratedGroup from "./generatedGroup.class";
+
+const getCollection = () => {
+  return MongoHelper.client.db("Cluster0").collection("generatedGroupsID");
+};
+
+const getyearSemesterCollection = () => {
+  return MongoHelper.client.db("Cluster0").collection("yearSemester");
+};
+
+export default class GeneratedGroupController {
+  public GeneratedGroupAdd = async (
+    req: Request,
+    res: Response
+  ): Promise<any> => {
+    const requestData = req.body;
+    const collection: any = getCollection();
+
+    const generatedgroup = new GeneratedGroup(requestData);
+
+    collection
+      .insertOne(generatedgroup)
+      .then(() => {
+        res
+          .status(200)
+          .send(responses.success(SuccessCodes.SUCCESSFULLY_DATA_ADDED));
+        res.end();
+      })
+      .catch((err: any) => {
+        console.error(err);
+        res.send(responses.failed(ErrorCodes.INTERNAL_ERROR, 400));
+      });
+  };
+
+  public deleteGroup = async (req: Request, res: Response): Promise<any> => {
+    const id = req.params.id;
+    const collection: any = getCollection();
+
+    collection
+      .deleteOne({ _id: new mongodb.ObjectId(id) })
+      .then(() => {
+        res.send(responses.success(SuccessCodes.SUCCESSFULLY_DATA_DELETED));
+      })
+      .catch((err: any) => {
+        console.error(err);
+        res.send(responses.failed(ErrorCodes.INTERNAL_ERROR));
+      });
+  };
+
+  public getGroupById = async (req: Request, res: Response): Promise<any> => {
+    const collection: any = getCollection();
+
+    collection
+      .findOne({ _id: req.params.id })
+      .then((data: any) => {
+        res.send(
+          responses.successWithPayload(
+            SuccessCodes.SUCCESSFULLY_DATA_RETRIVED,
+            data
+          )
+        );
+      })
+      .catch((err: any) => {
+        console.error(err);
+        res.send(responses.failed(ErrorCodes.INTERNAL_ERROR, 400));
+      });
+  };
+
+  public GeneratedGrouplist = async (
+    req: Request,
+    res: Response
+  ): Promise<any> => {
+    const collection: any = getCollection();
+
+    try {
+      collection.find({}).toArray((err: any, items: any[]) => {
+        if (err) {
+          console.error("Caught error", err);
+          res
+            .status(500)
+            .send(responses.failed(ErrorCodes.INTERNAL_ERROR, 400));
+          res.end();
+        } else {
+          // items = items.map(
+          //   (item: { _id: any; name: any; email: any; phone: any }) => {
+          //     return {
+          //       id: item._id,
+          //       name: item.name,
+          //     };
+          //   }
+          // );
+
+          res
+            .status(200)
+            .send(
+              responses.successWithPayload(
+                SuccessCodes.SUCCESSFULLY_DATA_RETRIVED,
+                items
+              )
+            );
+        }
+      });
+    } catch (err) {
+      console.error(err);
+      res.send(responses.failed(ErrorCodes.INTERNAL_ERROR, 400));
+    }
+  };
+}
